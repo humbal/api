c48bf4a90b0e2640f76f1ec21f3032feb087978f
Humbal Shahi <sh.humble@gmail.com>
2021-07-22 00:38:36 -0700

    removed unnecessary method

--- app/Http/Controllers/CoinCombinationController.php
+++ app/Http/Controllers/CoinCombinationController.php
@@ -38,43 +38,4 @@ class CoinCombinationController extends Controller {
     
         return $denomination_count_array;
     }
-
-    /**
-     * Get denomination value in string.
-     *  For instance, if denomination is 100 cent, it should be labeled as 'silver-dollar' 
-     *  and like wise other.
-     * 
-     * @param int $val
-     * @return string
-     */
-    protected function getDenominationLabel2($val) {
-        $label = '';
-        switch($val) {
-          case 100:
-            $label = 'silver-dollar';
-            break;
-    
-          case 50:
-            $label = 'half-dollar';
-            break;
-    
-          case 25:
-            $label = 'quarter';
-            break;
-    
-          case 10:
-            $label = 'dime';
-            break;
-    
-          case 5:
-            $label = 'nickel';
-            break;
-    
-          case 1:
-            $label = 'penny';
-            break;
-        }
-    
-        return $label;
-    }
 }
